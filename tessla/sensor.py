eed50a6dc2cebe755837e242d98f9ae6d130e7a2
Andreas Sandblåst <83435293+Andy-Sandy@users.noreply.github.com>
2023-04-20 17:16:00 +0200

    Latest code

--- tessla/sensor.py
+++ tessla/sensor.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import subprocess
+import logging
 
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import async_track_state_change
@@ -8,31 +9,32 @@ from homeassistant.helpers.event import async_track_state_change
 tessla_spec_file = "specification.tessla"
 tessla_jar_file = "tessla.jar"
 
-# Start the TeSSLa interpreter process with the given specification file.
-tessla_process = subprocess.Popen(
-    ["java", "-jar", tessla_jar_file, "interpreter", tessla_spec_file],
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    universal_newlines=True,
-)
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    async_add_entities([MySensor(hass)])
+    # Start the TeSSLa interpreter process with the given specification file.
+    tessla_process = subprocess.Popen(
+        ["java", "-jar", tessla_jar_file, "interpreter", tessla_spec_file],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    _LOGGER.debug("Tessla started")
+    async_add_entities([MySensor(hass, tessla_process)])
 
 
 class MySensor(Entity):
-    def __init__(self, hass):
+    def __init__(self, hass, process):
         self._state = None
         self._hass = hass
+        self.tessla = process
+        self.j = 1
 
         # Create a separate thread to read and print the TeSSLa output.
-        output_thread = threading.Thread(
-            target=self.async_update, args=[tessla_process]
-        )
+        output_thread = threading.Thread(target=self.vs2, argument=[self])
         output_thread.start()
-        output_thread.join()
 
         # Register a state change listener for the "sensor.random_sensor" entity
         async_track_state_change(
@@ -47,7 +49,18 @@ class MySensor(Entity):
     def state(self):
         return self._state
 
-    async def async_update(self, process):
+    def vs2(me):
+        i = 0
+        while True:
+            line = f"{i}\n"
+            _LOGGER.debug(f"vs: {i}")
+            me._state = str(i)
+            me.schedule_update_ha_state()
+            i = i + 1
+            time.sleep(1)
+
+    def vs(self):
+        process = self.tessla
         while True:
             line = process.stdout.readline()
             if not line:
@@ -59,19 +72,6 @@ class MySensor(Entity):
     async def _async_state_changed(self, entity_id, old_state, new_state):
         if new_state is None:
             return
-
-    # Send some integer values to the TeSSLa process using its standard input stream.
-    tessla_process.stdin.write("10: x = 2\n")
-    tessla_process.stdin.flush()
-    time.sleep(1)
-    # tessla_process.stdin.write("11: x = 4\n")
-    # tessla_process.stdin.flush()
-    # time.sleep(1)
-    # tessla_process.stdin.write("12: x = 6\n")
-    # tessla_process.stdin.flush()
-
-    # Close the input stream to signal that we're done sending input.
-    # tessla_process.stdin.close()
-
-    # Wait for the TeSSLa process to exit and for the output thread to finish.
-    tessla_process.wait()
+        # self.tessla.stdin.write(f"{self.j}: x = {self.j}\n")
+        # self.tessla.stdin.flush()
+        self.j = self.j + 1
